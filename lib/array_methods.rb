8edd30b82eeae734695aff41b64ae50541627754
Joseph  Louie <joseph161216@gmail.com>
2019-10-20 02:22:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  sorted_arr = array.sort
-  sorted_arr[-1]
+  array.max
 end
 
 def find_min_value(array)
-  sorted_arr = array.sort
-  sorted_arr[0]
+  array.min
 end
